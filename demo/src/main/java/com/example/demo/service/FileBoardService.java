bfd01481c95fa97b0a950a94f0e0d286dc6c8ce2
조영찬 <whdudcks98@naver.com>
2023-01-11 20:42:02 +0900

    FileBoard git save

---
+++ demo/src/main/java/com/example/demo/service/FileBoardService.java
@@ -0,0 +1,19 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.bean.FileBoardVO;
+import com.example.demo.bean.FileVO;
+
+public interface FileBoardService {
+
+    List<FileBoardVO> getFileBoardList();
+    FileBoardVO fileBoardDetail(int b_no);
+    int fileBoardInsert(FileBoardVO fileBoard);
+    int fileBoardUpdate(FileBoardVO fileBoard);
+    int fileBoardDelete(int bno);
+    //파일 업로드 메서드 추가
+    int fileInsert(FileVO file);
+    //파일 다운로드 메서드 추가
+    FileVO fileDetail(int b_no);
+}
